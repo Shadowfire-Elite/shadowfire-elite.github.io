370c945840b8c5b26c577cef670fd7a5e4e4a75a
Shadowfire Elite <tsid2181@gmail.com>
2021-08-04 14:42:51 -0400

    updated swiper

--- script.js
+++ script.js
@@ -23,6 +23,29 @@ $(document).ready(function () {
     $(window).scrollTop(0);
   });
 
+  // Swiper
+  var mySwiper = new Swiper(".swiper-container", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+  });
+
   // Stats counter
   $(".counter").counterUp({
     delay: 10,
