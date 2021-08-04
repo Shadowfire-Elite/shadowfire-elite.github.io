45f5b34978b0213d3333d425922a4b4f9518d4ce
Shadowfire Elite <tsid2181@gmail.com>
2021-08-04 17:12:51 -0400

    changed layout

--- script.js
+++ script.js
@@ -22,35 +22,6 @@ $(document).ready(function () {
   btn.click(function () {
     $(window).scrollTop(0);
   });
-
-  // Swiper
-  var mySwiper = new Swiper(".swiper-container", {
-    // Optional parameters
-    direction: "horizontal",
-    loop: true,
-
-    // If we need pagination
-    pagination: {
-      el: ".swiper-pagination",
-    },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-      el: ".swiper-scrollbar",
-    },
-  });
-
-  // Stats counter
-  $(".counter").counterUp({
-    delay: 10,
-    time: 1000,
-  });
   //   $(".testimonial-carousel").slick({
   //     infinite: true,
   //     slidesToShow: 1,
